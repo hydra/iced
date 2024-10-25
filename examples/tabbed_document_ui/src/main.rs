8536f8397ec7599a4d7a9cefacc75f4552117838
Dominic Clifton <me@dominicclifton.name>
2024-10-25 19:49:11 +0200

    Compilation fail

--- examples/tabbed_document_ui/src/main.rs
+++ examples/tabbed_document_ui/src/main.rs
@@ -2,8 +2,10 @@
 
 use iced_aw::style::tab_bar;
 use iced_aw::Tabs;
+use slotmap::{new_key_type, SlotMap};
 use iced::widget::{button, column, container, row, text};
 use iced::{Element, Task };
+use crate::home::HomeTab;
 
 /// entry point
 pub fn main() -> iced::Result {
@@ -12,24 +14,59 @@ pub fn main() -> iced::Result {
 
 #[derive(Debug, Clone)]
 enum Message {
+    None,
+    AddHome,
+}
+
+new_key_type! {
+    pub struct TabKey;
+}
+
+enum TabMessage {
     None
 }
 
-#[derive(PartialEq, Eq, Clone)]
-struct TabId(pub usize);
+trait Tab {
+    fn view(&self) -> Element<'static, TabMessage>;
+}
 
 #[derive(Default)]
 struct TabbedDocumentUI {
+    tabs: SlotMap<TabKey, Box<dyn Tab>>,
+}
+
+mod home {
+    use iced::Element;
+    use iced::widget::text;
+    use crate::{Tab, TabMessage};
+
+    #[derive(Default)]
+    pub struct HomeTab {}
+
+    impl Tab for HomeTab {
+        fn view(&self) -> Element<'static, TabMessage> {
+            let text = text("content area");
+
+            text.into()
+        }
+    }
 }
 
 impl TabbedDocumentUI {
     fn update(&mut self, _message: Message) -> Task<Message> {
+        match _message {
+            Message::None => {}
+            Message::AddHome => {
+                self.add_home()
+            }
+        }
         Task::none()
     }
 
     fn view(&self) -> Element<'_, Message> {
 
-        let home_button = button("home");
+        let home_button = button("home")
+            .on_press(Message::AddHome);
         let new_button = button("new");
         let open_button = button("open");
         let close_all_button = button("close all");
@@ -39,17 +76,17 @@ impl TabbedDocumentUI {
             row![home_button, new_button, open_button, close_all_button]
                 .into();
 
-        let tab_bar = Tabs::<Message, TabId>::new(|tab_id|{
+        let tab_bar = Tabs::<Message, TabKey>::new(|tab_id|{
             Message::None
         })
             .tab_icon_position(iced_aw::tabs::Position::Bottom)
             .on_close(|tab_id|{
                 Message::None
             })
-            .set_active_tab(&TabId(0))
+            //.set_active_tab(&TabId(0))
             .tab_bar_style(Box::new(tab_bar::primary));
 
-        let tab_bar: Element<'_, Message > = tab_bar
+        let tab_bar: Element<'_, Message> = tab_bar
             .into();
 
         let text = text("content area");
@@ -63,5 +100,10 @@ impl TabbedDocumentUI {
                 .into();
 
         container(ui).into()
+    }
+
+    fn add_home(&mut self) {
+        let home_tab = HomeTab::default();
+        let _key = self.tabs.insert(Box::new(home_tab));
     }
 }
