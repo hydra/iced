6d23df9e618bdbac7ee0207e12746eb1d49de954
Dominic Clifton <me@dominicclifton.name>
2024-10-29 13:35:29 +0100

    Display images.

--- examples/tabbed_document_ui/src/document/image.rs
+++ examples/tabbed_document_ui/src/document/image.rs
@@ -1,9 +1,10 @@
 use std::path::PathBuf;
-use iced::{Element, Length};
-use iced::widget::Space;
+use iced::{ContentFit, Element, Length};
+use iced::widget::{image, Space};
 
 pub struct ImageDocument {
     pub path: PathBuf,
+    handle: image::Handle
 }
 
 #[derive(Debug, Clone)]
@@ -14,18 +15,23 @@ pub enum ImageDocumentMessage {
 impl ImageDocument {
     pub fn new(path: PathBuf) -> Self {
         println!("creating image document. path: {:?}", path);
+
+        let handle = image::Handle::from_path(&path);
+
         Self {
             path,
+            handle
         }
     }
 
     pub fn view(&self) -> Element<'_, ImageDocumentMessage> {
-        let view = Space::new(
-            Length::Fill,
-            Length::Fill,
-        );
 
-        view
+        let image = image(&self.handle)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .content_fit(ContentFit::Contain);
+
+        image
             .into()
     }
 }
