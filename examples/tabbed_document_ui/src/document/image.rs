85e71be4d23a5abb503c7bd2b5f373d9838151dc
Dominic Clifton <me@dominicclifton.name>
2024-10-29 17:41:00 +0100

    Fail to get the image top/left justified.

--- examples/tabbed_document_ui/src/document/image.rs
+++ examples/tabbed_document_ui/src/document/image.rs
@@ -34,12 +34,29 @@ impl ImageDocument {
         let sidebar = self.sidebar.view()
             .map(|message|ImageDocumentMessage::None);
 
+        // FIXME the image should be:
+        //       * top-left justified
+        //       * maintain it's aspect ratio
+        //       * fill the available space on the shortest edge of the container
+        //       * have no whitespace to the left or of the image
+        //       * have no whitespace on the right of the image
+        //       However, no amount of fiddling with the .width/height/align methods makes it work.
+
         let image = image(&self.handle)
             .width(Length::Fill)
             .height(Length::Fill)
             .content_fit(ContentFit::Contain);
 
-        let ui = row![sidebar, image];
+        let image_container = container(image)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .align_x(Horizontal::Left)
+            .align_y(Vertical::Top)
+            // TODO why do these take a 'Length' argument?
+            .align_left(Length::Fill)
+            .align_top(Length::Fill);
+
+        let ui = row![sidebar, image_container];
 
         ui
             .into()
